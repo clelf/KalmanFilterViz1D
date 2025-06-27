c017ae10a1c084f92299bb58090d94dfcf95e672
clelf <clementinelvfd@gmail.com>
2025-06-27 18:16:36 +0200

    LGD formulation alternative

--- kalman.py
+++ kalman.py
@@ -1,62 +1,120 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# TODO: rewrite dynamics in terms of tau and d instead of a and b
+# TODO: how to define A
 
+def process_dynamics_A(A, B, Q, T, x0, s0):
+    # Generate the process's states with noise of var Q
+    process_state = np.zeros(T)
+    process_state[0] = x0
 
-def process_dynamics(A, Q, T, x0, s0):
-    # Generate the process's true states with noise of var Q
-    true_state = np.zeros(T)
-    true_state[0] = x0
-
-    true_mean = np.zeros(T)
-    true_mean[0] = x0
-    true_var = np.zeros(T)
-    true_var[0] = s0
+    process_mean = np.zeros(T)
+    process_mean[0] = x0
+    process_var = np.zeros(T)
+    process_var[0] = s0
     
     for t in range(1, T):
-        true_mean[t] = A * true_mean[t-1]
-        true_var[t] = Q
-        true_state[t] = A * true_state[t-1] + np.random.normal(0, np.sqrt(true_var[t]))
+        process_state[t] = A * process_state[t-1] + np.random.normal(0, np.sqrt(Q))
 
-    return true_state, true_mean, true_var
+        process_mean[t] = A * process_mean[t-1] + B
+        process_var[t] = Q
 
-def observ_dynamics(true_state, R, T):
+    return process_state, process_mean, process_var
+
+
+def process_dynamics_tau(tau, x_lim, Q, T, x0, s0):
+    # Generate the process's states with noise of var Q
+    process_state = np.zeros(T)
+    process_state[0] = x0
+
+    process_mean = np.zeros(T)
+    process_mean[0] = x0
+    process_var = np.zeros(T)
+    process_var[0] = s0
+    
+    for t in range(1, T):
+        process_state[t] = process_state[t-1] + 1 / tau * (x_lim - process_state[t-1]) + np.random.normal(0, np.sqrt(Q))
+
+        process_mean[t] =  process_mean[t-1] + 1 / tau * (x_lim - process_mean[t-1])
+        process_var[t] = Q 
+
+    return process_state, process_mean, process_var
+
+
+
+def observ_dynamics(process_state, C, R, T):
     # Generate observations (measurements) with noise of var R
     measurements = np.zeros(T)
     obs_mean = np.zeros(T)
     obs_var = np.zeros(T)
 
     for t in range(0, T):
-        obs_mean[t] = true_state[t]
+        measurements[t] = C * obs_mean[t] + np.random.normal(0, np.sqrt(obs_var[t]))
+
+        obs_mean[t] = C * process_state[t]
         obs_var[t] = R
-        measurements[t] = obs_mean[t] + np.random.normal(0, np.sqrt(obs_var[t]))
 
     return measurements, obs_mean, obs_var
 
-def kalman(measurements, A, Q, R, x0, s0):
+def kalman_A(measurements, A, B, C, Q, R, x0):
     T = len(measurements)
 
     # Kalman filter initialization
     x_est = np.zeros(T)  # Estimated states mean
     s_est = np.zeros(T)  # Estimated states variance
     x_est[0] = x0  # Start with the initial estimate
-    s_est[0] = s0
+    s_est[0] = 0
 
     for t in range(1, T):
         # Prediction step
-        x_pred = A * x_est[t-1]
+        x_pred = A * x_est[t-1] + B
         s_pred = A**2 * s_est[t-1] + Q
 
         # Update step (Kalman Gain)
         K = s_pred / (s_pred + R)
-        x_est[t] = x_pred + K * (measurements[t] - x_pred)
+        x_est[t] = C * x_pred + K * (measurements[t] - x_pred)
         s_est[t] = (1 - K) * s_pred 
 
     return x_est, s_est
 
-def plot_estim(process, obs, est_mu, est_var):
+def kalman_tau(measurements, tau, x_lim, C, Q, R, x0):
+    T = len(measurements)
+
+    # Kalman filter initialization
+    x_est = np.zeros(T)  # Estimated states mean
+    s_est = np.zeros(T)  # Estimated states variance
+    x_est[0] = x0  # Start with the initial estimate
+    s_est[0] = 0
+
+    for t in range(1, T):
+        # Prediction step
+        x_pred = x_est[t-1] + 1 / tau * (x_lim - x_est[t-1])
+        s_pred = (1 - 1 / tau)**2 * s_est[t-1] + Q
+
+        # Update step (Kalman Gain)
+        K = s_pred / (s_pred + R)
+        x_est[t] = C * x_pred + K * (measurements[t] - x_pred)
+        s_est[t] = (1 - K) * s_pred 
+
+    return x_est, s_est
+
+def kalman_batch(ys, parsets, C, Q, R, x0s):
+    y_hats, s_hats = [], []
+    for y, parset, y0 in zip(ys, parsets, x0s):
+        y_hat, s_hat = kalman_tau(y, *parset, C, Q, R, y0)
+        y_hats.append(y_hat)
+        s_hats.append(s_hat)
+    return np.stack([batch for batch in y_hats], axis=0), np.stack([batch for batch in s_hats], axis=0)
+
+
+
+def plot_estim(obs, est_mu, est_var, process=None):
+    """ process is not necessarily known --> can be set to None
+    """
     plt.figure(figsize=(10, 6))
-    if process is not None: plt.plot(range(len(process)), process, label='True State', color='green', linewidth=2)
+    if process is not None: 
+        plt.plot(range(len(process)), process, label='True State', color='green', linewidth=2)
     plt.plot(range(len(obs)), obs, label='Noisy Measurements', color='red', linestyle='dotted')
     plt.plot(range(len(est_mu)), est_mu, label='Kalman Filter Estimate', color='blue', linewidth=2)
     plt.fill_between(range(len(est_var)), est_mu-np.sqrt(est_var), est_mu+np.sqrt(est_var), color='blue', alpha=0.2)
@@ -70,18 +128,19 @@ if __name__=='__main__':
 
     # Define system parameters
     A = 1  # State transition matrix
+    B = 0 # B = x_lim / tau
+    C = 1 # Observation model
     Q = 0.1  # Process noise covariance
     R = 1  # Measurement noise covariance
     x0 = 0.0  # Initial state mean
-    s0 = 2 # Initial state var
     T = 50  # Number of time steps
 
     # Generate data
-    process,_,_ = process_dynamics(A, Q, T, x0, s0)    
-    obs,_,_ = observ_dynamics(process, Q, T)
+    process,_,_ = process_dynamics_A(A, B, Q, T, x0)    
+    obs,_,_ = observ_dynamics(process, C, Q, T)
 
     # Estimate with Kalman filter
-    x_est, s_est = kalman(obs, A, Q, R, x0, s0)
+    x_est, s_est = kalman_A(obs, A, B, C, Q, R, x0)
     
     # Plot the results
     plot_estim(process, obs, x_est, s_est)
